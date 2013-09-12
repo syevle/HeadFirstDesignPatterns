6768b9f0e7594f2aa94124da2e67191ff44a27ef
syevle <syevle@gmail.com>
2013-09-12 12:49:40 +0530

    tt

--- HF_DP/src/web/ObserverPattern/org/arpit/javapostsforlearning/Observer.java
+++
@@ -1,5 +0,0 @@
-package web.ObserverPattern.org.arpit.javapostsforlearning;
-
-public interface Observer {
-	  public void update(String availability);
-}
--- HF_DP/src/web/ObserverPattern/org/arpit/javapostsforlearning/Subject.java
+++
@@ -1,7 +0,0 @@
-package web.ObserverPattern.org.arpit.javapostsforlearning;
-
-public interface Subject {
-	 public void registerObserver(Observer observer);
-     public void removeObserver(Observer observer);
-     public void notifyObservers();
-}
