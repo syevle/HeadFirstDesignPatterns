c44904d013f58661343b374b5ce603e6dbd9f367
syevle <syevle@gmail.com>
2013-09-12 14:52:16 +0530

    tt

---
+++ HF_DP/src/web/ObserverUsingJavaUtil/NameObserver.java
@@ -0,0 +1,22 @@
+package web.ObserverUsingJavaUtil;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class NameObserver implements Observer {
+	private String name;
+
+	public NameObserver() {
+		name = null;
+		System.out.println("NameObserver created: Name is " + name);
+	}
+
+	public void update(Observable obj, Object arg) {
+		if (arg instanceof String) {
+			name = (String) arg;
+			System.out.println("NameObserver: Name changed to " + name);
+		} else {
+			System.out.println("NameObserver: Some other change to subject!");
+		}
+	}
+}
---
+++ HF_DP/src/web/ObserverUsingJavaUtil/PriceObserver.java
@@ -0,0 +1,22 @@
+package web.ObserverUsingJavaUtil;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class PriceObserver implements Observer {
+	private float price;
+
+	public PriceObserver() {
+		price = 0;
+		System.out.println("PriceObservercreated: Price is " + price);
+	}
+
+	public void update(Observable obj, Object arg) {
+		if (arg instanceof Float) {
+			price = ((Float) arg).floatValue();
+			System.out.println("PriceObserver: Price changed to " + price);
+		} else {
+			System.out.println("PriceObserver: Some other change to subject!");
+		}
+	}
+}
