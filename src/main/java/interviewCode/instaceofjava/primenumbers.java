f29f4e607d01b4038b2f6589fa02b640dd7cbfa5
Santosh Yevle <santosh.yevle@reachlocal.com>
2017-06-17 17:26:41 +0530

    r3r3

---
+++ src/main/java/interviewCode/instaceofjava/primenumbers.java
@@ -0,0 +1,56 @@
+package interviewCode.instaceofjava;
+
+/**
+ * Created by Santosh on 6/1/17.
+ */
+public class primenumbers {
+
+    public static void main(String[] args) {
+
+        int num=50;
+        int count=0;
+
+        for(int i=2;i<=num;i++){
+
+            count=0;
+
+            for(int j=2;j<=i/2;j++){
+
+                if(i%j==0){
+                    count++;
+                    break;
+                }
+
+            }
+
+            if(count==0){
+
+                System.out.println(i);
+
+            }
+
+        }
+
+    }
+
+}
+
+/*
+============================OutPut=============================
+2
+3
+5
+7
+11
+13
+17
+19
+23
+29
+31
+37
+41
+43
+47
+
+ */
